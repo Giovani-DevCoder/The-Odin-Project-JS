6999f434940b37342f34306c3d3da262323f378a
Giovani-DevCoder <DGSamuel255@Outlook.com>
2026-06-22 14:32:20 -0400

    First problem

---
+++ 01.ObjectPractice.js
@@ -0,0 +1,32 @@
+/*const playerOne = {
+    name: "Markus",
+    marker: "X"
+};
+
+const playerTwo = {
+    name: "Connor",
+    marker: "O"
+};
+
+function gameOver(winningPlayer) {
+    console.log("Game Over!");
+    console.log(`${winningPlayer.name} ${winningPlayer.marker} is the winner!`);
+} */
+
+const car = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2020,
+    color: "Blue",
+    price: 2000,
+    applyDiscount: function(discount){
+        multiplier = 1 - (discount / 100);
+        this.price *= multiplier
+    },
+    
+    getSummary(){
+        return `the ${car.model} ${car.make} ${car.color} of the year ${car.color} is priced at ${car.price}`
+    }
+};
+
+console.log(car.getSummary(car.applyDiscount(50)))
