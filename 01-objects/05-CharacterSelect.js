a354e6e17e7be1ae8789c71ed882125f2e724834
Giovani-DevCoder <DGSamuel255@Outlook.com>
2026-06-24 09:34:16 -0400

    Prototipado

--- 01-objects/05-CharacterSelect.js
+++ 01-objects/05-CharacterSelect.js
@@ -4,20 +4,7 @@ function Hero(name, level){
     this.level = level;
 }
 
-/* const hero1 = new Hero("Maia", 1)
-
-console.log(hero1)
 console.log("--------------")
-
-Hero.prototype.Greet = function() {
-    return `${this.name}, that's my name!`;
-}
-
-const greeting = hero1.Greet()
-
-console.log(greeting) */
-console.log("--------------")
-
 function Warrior(name, level, weapon){
     Hero.call(this, name, level);
     this.weapon = weapon;
@@ -28,17 +15,34 @@ function Mage(name, level, spell){
     this.spell = spell;
 };
 
+// Link prototypes and add prototype methods
+Object.setPrototypeOf(Warrior.prototype, Hero.prototype);
+Object.setPrototypeOf(Mage.prototype, Hero.prototype);
+
+Hero.prototype.Greet = function() {
+    return `${this.name}, that's my name!`;
+};
+
 Warrior.prototype.Attack = function(){
-    return `${this.name} attack the enemy with a ${this.weapon}`;
-}
+    return `${this.name} attacks with a ${this.weapon}`;
+};
 
 Mage.prototype.Spell = function(){
-    return `${this.name} cast a ${this.spell}`;
-}
+    return `${this.name} casts a ${this.spell}`;
+};
 
+// Initialize individual character instances
 const hero1 = new Warrior("Maia", 1, "Sword")
 const hero2 = new Mage("Frieren", 1, "Spell")
 
 
 console.log(hero1)
 console.log(hero2)
+console.log("--------------")
+
+const act = hero1.Attack()
+const greeting = hero1.Greet()
+
+console.log(greeting)
+console.log("--------------")
+console.log(act)
